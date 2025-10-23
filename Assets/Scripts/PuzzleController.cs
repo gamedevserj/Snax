e2ededad145408303968fa1c384852d98b9c7677
serj <13497925+gamedevserj@users.noreply.github.com>
2025-10-23 10:39:22 +0400

    Setup puzzle controller

---
+++ Assets/Scripts/PuzzleController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PuzzleController : MonoBehaviour
+{
+
+    [SerializeField] private GridController _gridController;
+    [SerializeField] private Transform _cellHighlight;
+    [SerializeField] private KeyCode _up = KeyCode.W;
+    [SerializeField] private KeyCode _down = KeyCode.S;
+    [SerializeField] private KeyCode _left = KeyCode.A;
+    [SerializeField] private KeyCode _right = KeyCode.D;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(_up))
+        {
+            Move(Vector3.up);
+        }
+        if (Input.GetKeyDown(_down))
+        {
+            Move(Vector3.down);
+        }
+        if (Input.GetKeyDown(_left))
+        {
+            Move(Vector3.left);
+        }
+        if (Input.GetKeyDown(_right))
+        {
+            Move(Vector3.right);
+        }
+    }
+
+    private void Move(Vector3 direction)
+    {
+        // keeping it within the grid
+        if (_cellHighlight.position.x + direction.x < 0
+            || _cellHighlight.position.y + direction.y < 0
+            || _cellHighlight.position.x + direction.x > _gridController.GridSize.x - 1
+            || _cellHighlight.position.y + direction.y > _gridController.GridSize.y - 1)
+        {
+            return;
+        }
+
+        _cellHighlight.position += direction;
+    }
+
+}
